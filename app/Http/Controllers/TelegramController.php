99f4ede115231401591b428ff75229b746ef2f82
WorakornX <worakorn.wattanakulchart@gmail.com>
2018-06-20 05:14:06 +0700

    sdf

--- app/Http/Controllers/TelegramController.php
+++ app/Http/Controllers/TelegramController.php
@@ -14,7 +14,7 @@ class TelegramController extends Controller
 
         $msg = array();
         // $msg['username'] = ['message']['from']['username'];
-        $msg['firstname'] = ['message']['from']['first_name'];
+        $msg['firstname'] = ['message_id'];
         // $msg['bot'] = ['message']['from']['is_bot'];
         // $msg['text'] = ['message']['chat']['text'];
 
